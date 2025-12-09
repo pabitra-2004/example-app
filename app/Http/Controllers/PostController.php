1c9a39e2cf61037e313e3ff21294ebc87f18f7b6
Souvik Das <souvikdas.dev@gmail.com>
2025-12-09 08:59:59 +0530

    wip

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,22 +13,28 @@ class PostController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $posts = Post::all(); // select * from posts;
-        $postIds = $posts->pluck('id')->all(); // extract all post ids from $posts
-        // dd($postIds);
-        $comments = Comment::whereIn('post_id', $postIds)->get(); // select * from comments where post_id in [?,?,?,?,...]; 
+        /**
+         * another way**/
 
-        // map every post with their respective comments
-        $posts->every(function ($post, $key) use ($comments) { 
-            // extract respectie comments and add as an attribute named 'comments'
-            return $post->comments = $comments->where('post_id', $post->id);
-        });
+        // $posts = Post::all(); // select * from posts;
+        // $postIds = $posts->pluck('id')->all(); // extract all post ids from $posts
+        // // dd($postIds);
+        // $comments = Comment::whereIn('post_id', $postIds)->get(); // select * from comments where post_id in [?,?,?,?,...]; 
+
+        // // map every post with their respective comments
+        // $posts->every(function ($post, $key) use ($comments) { 
+        //     // extract respectie comments and add as an attribute named 'comments'
+        //     return $post->comments = $comments->where('post_id', $post->id);
+        // });
 
         // dd($posts, $comments);
 
         // foreach ($posts as $post) {
         //     $comments = Comment::where('post_id', $post->id)->get(); // select * from comments where post_id = ?;
         // }
+
+        // best way
+        $posts = Post::with('comments')->get();
 
         return view('posts.index', ['posts' => $posts]);
     }
