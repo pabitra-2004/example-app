235e645a6ea88c1e459af588d6a4bdb1b8543d1a
Pabitra Das <pabitra4375@gmail.com>
2026-01-13 15:55:17 +0530

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,37 +9,13 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="p-6">
+<body class="px-6">
 
-    {{-- * Alert component --}}
-    <x-alert type="danger" message="Thsi is a normal a danger" />
-    <x-alert type="warning" message="This is a dismissable warning." dismissable />
-    <x-alert type="warning" message="This is a dismissable and auto dismissable warning." dismissable
-        auto-dismissable />
-    <x-alert type="success" message="This is a dimissable, auto dismissable with dely time success " dismissable
-        auto-dismissable auto-dismiss-delay="5000" />
-
-    <hr class="my-4">
-
-    <x-dropdown position="left" title="Pabitra Das">
-        <x-dropdown.item>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-6 inline-block">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-            Account
-        </x-dropdown.item>
-        <x-dropdown.item>Profile</x-dropdown.item>
-        <x-dropdown.item>Billing</x-dropdown.item>
-        <x-dropdown.item>Logout</x-dropdown.item>
-        <x-dropdown.item>Delete</x-dropdown.item>
-    </x-dropdown>
-
-    <hr class="my-4">
-
+    {{-- <hr class="my-2"> --}}
     {{-- * Accordion component --}}
-    <div x-data="{
+    {{-- <h1 class="m-2 text-lg font-semibold text-center text-blue-500">Accordion component</h1> --}}
+    {{-- ? Accordion component method 1 --}}
+    {{-- <div x-data="{
         showId: null,
     }">
         <div class="m-auto accordion">
@@ -49,50 +25,11 @@
             <x-accordionx id="4" title="Accordion Item #4" />
             <x-accordionx id="5" title="Accordion Item #" />
         </div>
-    </div>
+    </div> --}}
 
-    <div class="w-3xl mx-auto my-20">
 
-        {{-- accordion component --}}
-        <div x-data="{
-            show: false,
-        }" class="border border-gray-300 rounded overflow-hidden">
-            {{-- accordion item --}}
-            <div>
-                <h2 class="accordion-header">
-                    <button type="button" x-on:click="show = !show"
-                        class="flex items-center justify-between w-full p-4 text-left bg-gray-100 border border-gray-200">
-                        Accordion Item #1
-
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="size-6 transition duration-300 ease-in-out" :class="{ '-rotate-180': show }">
-                            <path fill-rule="evenodd"
-                                d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </button>
-                </h2>
-                <div x-show="show" x-collapse.duration.1000ms>
-                    <div class="p-4">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the
-                        collapse
-                        plugin adds the appropriate classes that we use to style each element. These classes control the
-                        overall
-                        appearance, as well as the showing and hiding via CSS transitions. You can modify any of this
-                        with
-                        custom CSS or overriding our default variables. It's also worth noting that just about any HTML
-                        can
-                        go
-                        within the <code>.accordion-body</code>, though the transition does limit overflow.
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="w-3xl mx-auto my-20">
-
+    {{-- ? accordion component method 2 --}}
+    <div class="mx-auto my-10 w-3xl">
         <x-accordion>
             <x-accordion.item title="Accordion Item #1">
                 <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse
@@ -126,82 +63,131 @@
 
     </div>
 
-    <hr class="my-4">
-
-    <x-breadcrumb separator="/" :items="[['name' => 'Home', 'url' => '/'], ['name' => 'Library', 'url' => '/library'], ['name' => 'Data']]" />
 
 
-    <hr class="my-4">
+    {{-- <hr class="my-2"> --}}
+    {{-- * Alert component --}}
+    {{-- <h1 class="m-2 text-lg font-semibold text-center text-blue-500">Alert component</h1>
+    <x-alert type="danger" message="Thsi is a normal a danger" />
+    <x-alert type="warning" message="This is a dismissable warning." dismissable />
+    <x-alert type="warning" message="This is a dismissable and auto dismissable warning." dismissable
+        auto-dismissable />
+    <x-alert type="success" message="This is a dimissable, auto dismissable with dely time success " dismissable
+        auto-dismissable auto-dismiss-delay="5000" /> --}}
 
 
+
+    <hr class="my-2">
+    {{-- * Breadcrumb component --}}
+    <h1 class="m-2 text-lg font-semibold text-center text-blue-500">Breadcrumb component</h1>
+    <x-breadcrumb separator="/" :items="[['name' => 'Home', 'url' => '/'], ['name' => 'Library', 'url' => '/'], ['name' => 'Data']]" />
+
+
+
+    <hr class="my-2">
     {{-- * dropdown component --}}
+    <h1 class="m-2 text-lg font-semibold text-center text-blue-500 cursor-pointer">Dropdown component</h1>
+    <x-dropdown position="bottom" title="Pabitra Das">
+        <x-dropdown.item>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="inline-block size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+            Account
+        </x-dropdown.item>
+        <x-dropdown.item>Profile</x-dropdown.item>
+        <x-dropdown.item>Billing</x-dropdown.item>
+        <x-dropdown.item>Logout</x-dropdown.item>
+        <x-dropdown.item>Delete</x-dropdown.item>
+    </x-dropdown>
 
-    <div>
-        <button type="button" class="flex items-center gap-2 hover:bg-gray-200 p-2 rounded-lg w-fit">
-            <h1>Pabitra Das</h1>
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-                    <path fill-rule="evenodd"
-                        d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-                        clip-rule="evenodd" />
-                </svg>
 
-            </div>
+
+
+
+    <hr class="my-2">
+    {{-- * Modal component --}}
+    <h1 class="m-2 text-lg font-semibold text-center text-blue-500">Modal component</h1>
+
+    <div x-data="{
+        open: false,
+    }">
+        <button x-on:click="open = !open"
+            class="text-white bg-blue-600 hover:bg-blue-700  font-semibold rounded-lg text-sm px-4 py-2.5 shadow-lg cursor-pointer">
+            Toggle modal
         </button>
-        <ul class="flex flex-col space-y-2">
-            <li class="flex items-center gap-2">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-                        <path
-                            d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
-                    </svg>
-                </div>
-                <span>Account</span>
-            </li>
-            <li class="flex items-center gap-2">
-                <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-                        <path fill-rule="evenodd"
-                            d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z"
-                            clip-rule="evenodd" />
-                        <path fill-rule="evenodd"
-                            d="M1 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H1.75A.75.75 0 0 1 1 10Z"
-                            clip-rule="evenodd" />
-                    </svg>
 
+
+        <div x-show="open" x-transition.duration.300ms class="fixed inset-0 z-50 flex items-center justify-center size-full bg-black/50">
+
+            {{-- method 1 --}}
+            {{-- <dialog class="">
+            </dialog> --}}
+
+            {{-- method 2 --}}
+            <div class="p-8 overflow-x-hidden overflow-y-auto bg-gray-300 border border-gray-400 shadow-lg select-none w-md rounded-xl">
+                <div class="flex items-center justify-between pb-3 border-b border-gray-400">
+                    <h3 class="text-lg font-medium text-gray-900">
+                        Sign in to our platform
+                    </h3>
+                    <button type="button" x-on:click="open = !open"
+                        class="p-2 text-gray-700 transition-all duration-300 ease-in-out rounded-lg cursor-pointer hover:bg-gray-500 hover:text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path
+                                d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+                        </svg>
+                    </button>
                 </div>
-                <span>Logout</span>
-            </li>
-        </ul>
+
+                <form action="#" class="pt-6">
+                    <div class="mb-4">
+                        <label for="email" class="block mb-2.5 text-sm font-medium text-gray-900">Your
+                            email</label>
+                        <input type="email"
+                            class="bg-gray-100 border border-gray-400 text-gray-900 text-sm rounded-lg  block w-full px-3 py-2.5 shadow-xs focus:outline placeholder:text-gray-600"
+                            placeholder="example@gmail.com" required />
+                    </div>
+
+                    <div>
+                        <label for="password" class="block mb-2.5 text-sm font-medium text-gray-900">Your
+                            password</label>
+                        <input type="password"
+                            class="bg-gray-100 border border-gray-400 text-gray-900 text-sm rounded-lg  block w-full px-3 py-2.5 shadow-xs focus:outline placeholder:text-gray-600"
+                            placeholder="•••••••••" required />
+                    </div>
+
+                    <div class="flex items-start my-6">
+                        <div class="flex items-center">
+                            <input id="checkbox-remember" type="checkbox"
+                                class="w-4 h-4 bg-gray-100 border border-gray-400 rounded-xs">
+                            <label for="checkbox-remember"
+                                class="text-sm font-medium text-gray-900 select-none ms-2">Remember
+                                me</label>
+                        </div>
+                        <a href="#" class="text-sm font-medium text-blue-700 ms-auto hover:underline">Forget
+                            Password?</a>
+                    </div>
+
+                    <button type="submit"
+                        class="text-white transition-all duration-300 ease-in-out bg-blue-600 border border-blue-800 hover:bg-blue-700 shadow-xs font-medium rounded-lg text-sm px-4 py-2.5 w-full mb-3">Login
+                        to your account
+                    </button>
+
+                    <div class="text-sm font-medium text-gray-900">
+                        Not registered?
+                        <a href="#" class="text-blue-700 hover:underline">Create account</a>
+                    </div>
+                </form>
+            </div>
+
+        </div>
 
     </div>
+    <hr class="my-2">
 
 
-    {{-- * Modal component --}}
-    {{-- <div x-data="{
-        show: true,
-    }">
-        <button x-on:click="show = !show"
-            class="px-6 py-2 font-semibold text-white transition-all duration-300 ease-in-out bg-blue-600 rounded-lg shadow-lg cursor-pointer hover:bg-blue-700">Modal</button>
 
-        <div x-show="show" x-transition
-            class="flex flex-col items-center justify-center p-4 space-y-6 bg-gray-300 rounded-lg shadow-lg w-xs">
-            <div class="w-full text-2xl font-bold tracking-normal text-center text-gray-800 form-header">Login Form
-            </div>
-            <div class="flex flex-col w-full space-y-2 form-body">
-                <input type="text"
-                    class="w-full px-2.5 py-1 border rounded placeholder:font-medium text-sm focus:outline-none"
-                    placeholder="Enter the email">
-                <input type="password" class="w-full rounded border px-2.5 py-1 focus:outline-none"
-                    placeholder="Enter the password">
-            </div>
-            <div class="flex justify-end w-full gap-4 font-semibold text-white">
-                <input type="reset" value="Close"
-                    class="px-5 py-1.5  bg-yellow-600 rounded-lg shadow-lg cursor-pointer hover:bg-yellow-700">
-                <input type="submit" value="Login"
-                    class="px-5 py-1.5  bg-blue-600 rounded-lg shadow-lg cursor-pointer hover:bg-blue-700">
-            </div>
-        </div>
-    </div> --}}
 </body>
 
 </html>
